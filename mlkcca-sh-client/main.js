777cf4750f0dfcf03bcede2ccd991f3243f1783d
Toshiya Kawasaki <pi@raspberrypi.(none)>
2015-07-29 17:13:36 +0000

    Initial Commit

---
+++ mlkcca-sh-client/main.js
@@ -0,0 +1,28 @@
+var milkcocoa = new MilkCocoa('guitaricoz8m9j.mlkcca.com');
+var chatDataStore = milkcocoa.dataStore('mlkcca-sh');
+var textArea, board;
+window.onload = function(){
+    textArea = document.getElementById("msg");
+    board = document.getElementById("board");
+}
+
+function clickEvent(){
+    var text = textArea.value;
+    sendText(text);
+}
+
+function sendText(text){
+    chatDataStore.push({message : text, type:"request"});
+    console.log("送信完了!");
+    textArea.value = "";
+}
+
+chatDataStore.on("push",function(data){
+    addText(data.value.message);
+});
+
+function addText(text){
+    var msgDom = document.createElement("li");
+    msgDom.innerHTML = text;
+    board.insertBefore(msgDom, board.firstChild);
+}
